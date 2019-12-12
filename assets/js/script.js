9b76a2c9b872f0f09dc00f296f3b36469ff69f09
skuttenkuler <samkiddmusic@gmail.com>
2019-12-11 19:44:56 -0800

    test

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,21 @@
-apiKey = "c16c4a8fdb9c60761ec51031deb88c71";
+
+
+$(document).ready(function(){
+
+    let location = $("#location").val();
+    var WeatherAPIKey = "c16c4a8fdb9c60761ec51031deb88c71";
+    var queryURL ="http://api.openweathermap.org/data/2.5/weather?q=" + location + "&units=imperial&appid=" + WeatherAPIKey;
+    console.log(location);
+    //console.log(city);
+    function getData(){
+        
+        console.log(location)
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+        }).then(function(response){
+            console.log(response)
+        });
+    }
+    $(".submit").on("click", getData);
+});
